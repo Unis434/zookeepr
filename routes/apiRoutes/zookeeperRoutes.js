04db2eb016557e0f219620c10f9a623fff57b0c7
Unis434 <unisk434@gmail.com>
2022-03-23 21:18:42 +0000

    add zookeeper functionality

---
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { filterByQuery, findById, createNewZookeeper, validateZookeeper } = require('../../lib/zookeepers');
+const { zookeepers } = require('../../data/zookeepers');
+
+router.get('/zookeepers', (req, res) => {
+    let results = zookeepers;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+router.get('/zookeepers/:id', (req, res) => {
+    const result = findById(req.params.id, zookeepers);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+})
+
+router.post('/zookeepers', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = zookeepers.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateZookeeper(req.body)) {
+        res.status(400).send('The zookeepers is not properly formatted.');
+    } else {
+        const animal = createNewZookeeper(req.body, zookeepers);
+        res.json(animal);
+    }
+})
+
+module.exports = router;
